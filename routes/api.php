633462c8c19ff9ccf8522ddac0e3540e19e161ba
Bariq47 <bariqrahmafebriawan@gmail.com>
2025-04-11 18:39:53 +0700

    Menambahkan implementasi JWT auth

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,8 +16,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
 });
 
-Route::apiResource('/students',StudentController::class);
+Route::middleware('auth:api')->group(function () {
+    Route::apiResource('/students', StudentController::class);
+});
+
